1eaefee2be39394533c7fc0d008185a5563c8b7c
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-26 12:27:15 +0900

    [chore]: [router 수정] router 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import GlobalStyle from './style/globalstyle'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
