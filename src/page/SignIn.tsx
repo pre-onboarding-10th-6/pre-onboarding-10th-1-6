aaee71d6c56449f66f683377bc7e054cf35183fe
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-26 10:14:39 +0900

    [feat]: [회원가입, 로그인 페이지 기본 퍼블리싱] 회원가입, 로그인 페이지 기본 퍼블리싱

--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function SignIn() {
-  return <div>s</div>
+  return (
+    <div>
+      <input data-testid="email-input" />
+      <input data-testid="password-input" />
+      <button data-testid="signin-button" type="button">
+        로그인
+      </button>
+    </div>
+  )
 }
--- src/page/SignUp.tsx
+++ src/page/SignUp.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function SignUp() {
-  return <div>s</div>
+  return (
+    <div>
+      <input data-testid="email-input" />
+      <input data-testid="password-input" />
+      <button data-testid="signup-button" type="button">
+        회원가입
+      </button>
+    </div>
+  )
 }
